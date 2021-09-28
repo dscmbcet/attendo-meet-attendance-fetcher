6fd1db98a62065c5050de67ded1f1a06fbdd884c
M Aswin Kishore <mak26.work@gmail.com>
2021-09-28 18:52:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Attendo Meet Attendance Fetcher
+
+Fetches attendance of a given meet ID every 5mins and saves the attendance on a sheet
+
+![](https://raw.githubusercontent.com/mak626/attendo/a5939dcef187f33c2b3111349646e748bd616382/icon.svg)
+
+Made in collaboration with Team Attendo
+
+[Attendo Repository](https://github.com/mak626/attendo)
+
+## .env Config
+
+`API_URL`
---
+++ index.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const readline = require("readline-sync");
+const cron = require("node-cron");
+const fetch = require("node-fetch");
+const fs = require("fs");
+require("colors");
+
+const attendees = new Set();
+const minutes = 1; //Set Greater than 2
+
+const date = new Date();
+const dateString = `${date.getDate()}-${
+  date.getMonth() + 1
+}-${date.getFullYear()}`;
+
+const fetchAttendance = async () => {
+  const URL = `${process.env.API_KEY}&id=${meetID}`;
+  const res = await fetch(URL);
+  const attendance = (await res.json()).attendance;
+  if (attendance) {
+    attendance.forEach((e) => attendees.add(e));
+    fs.writeFileSync(
+      `${meetID} ${dateString}.csv`,
+      `Name\n${[...attendees].join("\n")}`,
+      { flag: "w" }
+    );
+    console.log(
+      `${new Date().toLocaleTimeString()}`,
+      `: Fetched Attendance Now`.blue.bold
+    );
+  }
+};
+
+const meetID = readline
+  .question("Enter the meet ID / link: ".yellow.red)
+  .replace("https://meet.google.com/", "");
+
+if (meetID === "") {
+  console.error("Enter ID Properly".red.bold);
+  return;
+}
+
+console.log(`Fetching attendance every ${minutes} minutes`.green.bold);
+fetchAttendance();
+cron.schedule(`*/${minutes} * * * *`, () => {
+  try {
+    fetchAttendance();
+  } catch (e) {
+    console.error("Some error occured".red);
+  }
+});
